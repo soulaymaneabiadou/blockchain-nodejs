6e4faa9a737957769132781ccb278ec5e12d7934
Soulaymane Abiadou <46084869+soulaymaneabiadou@users.noreply.github.com>
2021-03-29 21:06:51 +0100

    Cleaned the code structure, added subing and publishing to the pubsub & implmeneted chain syncing across nodes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const axios = require('axios');
 const Blockchain = require('./blockchain');
+const PubSub = require('./app/pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 5000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(express.json());
 
@@ -14,9 +20,36 @@ app.post('/api/mine', (req, res, next) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.status(201).redirect('/api/blocks');
 });
 
-const PORT = 5000;
+const syncChains = async () => {
+  try {
+    const res = await axios.get(`${ROOT_NODE_ADDRESS}/api/blocks`);
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    const rootChain = res.data;
+
+    console.log('Replace chain on a sync with', rootChain);
+    blockchain.replaceChain(rootChain);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, async () => {
+  console.log(`Server listening on port ${PORT}`);
+
+  if (PORT !== DEFAULT_PORT) {
+    await syncChains();
+  }
+});
